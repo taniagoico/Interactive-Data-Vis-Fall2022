8e492bfbae9cec04d41a415a4ea59370c1bab6f8
Tania Goicoechea <tgoicoechea@gradcenter.cuny.edu>
2022-11-15 18:26:17 -0500

    commit1.3

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,52 @@
-console.log('hello world');
+
+//TUTORIAL 1.3
+
+let clicks = 0
+let clickCounter = () => {
+    clicks = clicks +1;
+    document.getElementById("counter").innerHTML = clicks;
+}
+
+
+
+
+//NOTES FROM CLASS//
+
+// console.log('hello world');
+
+// // console.log(document)
+// // console.log(document.getElementById("name-input"))
+// console.log(window)
+
+// const input = document.getElementById("name-input")
+
+// const updateName = () => {
+//     const userName = input.value;
+//     window.alert(`Bienvenide a la clase, ${userName}`)
+// }
+
+// let changeable = true;
+// const constant = true;
+
+// let counter = 0;
+// // console.log(counter)
+
+// function change() {
+//     let changeable = false;
+//     const constant = false;
+//     // console.log(changeable, constant)
+//     counter = counter + 1;
+// }
+// // change();change();
+// // console.log(counter)
+
+// // console.log(changeable, constant)
+
+// const array = ["a", "b", "c"]
+// const returnedArray = array.map(d => {
+//     // console.log("This value is " + d)
+//     return "This value is " + d
+// })
+// console.log(returnedArray);
+
+// const filtered
